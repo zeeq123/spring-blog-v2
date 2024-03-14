5bc074e65a1d08311ddee25f0a47c40fb84eacd5
kjs <zeeq125@gmail.com>
2024-03-14 12:24:27 +0900

    findAll V2 연습

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -11,6 +12,34 @@ import java.util.List;
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+
+    public List<Board> findAllV2(){
+        String q1 = "select b from Board b order by b.id desc";
+        List<Board> boardList = em.createQuery(q1 , Board.class).getResultList();
+
+        int[] userIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+
+        String q2 = "select u from User u where u.id in (";
+        for (int i = 0; i < userIds.length ; i++) {
+            if (i == userIds.length - 1){
+                q2 = q2 + userIds[i] + ")";
+            }else {
+                q2 = q2 + userIds[i] + ",";
+            }
+        }
+        List<User> userList = em.createQuery(q2 , User.class).getResultList();
+
+        for (Board board : boardList){
+            for (int i = 0; i < userList.size(); i++) {
+                User user = userList.get(i);
+                if (user.getId() == board.getUser().getId()){
+                    board.setUser(user);
+                }
+            }
+        }
+
+        return boardList; // user가 채워져 있어야함.
+    }
 
     public List<Board> findAll(){
         Query query = em.createQuery("select b from Board b order by b.id desc", Board.class);
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Import(BoardRepository.class)
 @DataJpaTest
 public class BoardRepositoryTest {
@@ -14,11 +16,53 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Test
+    public void findAllV2_test() {
+        List<Board> boardList = boardRepository.findAllV2();
+        System.out.println(boardList);
+    }
+
+    @Test
+    public void randomquery_test(){
+        int[] ids = {1,2};
+
+        // select u from User u where u.id in (:id1, :id2)
+        String q = "select u from User u where u.id in (";
+        for (int i = 0; i < ids.length; i++) {
+            if (i == ids.length-1){
+                q = q + ":id)";
+            }else {
+                q = q + ":id,";
+            }
+        }
+        System.out.println(q);
+    }
+
+    @Test
+    public void findAll_custom_test() {
+        List<Board> boardList = boardRepository.findAll();
+
+        int[] userIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+        for (int i : userIds){
+            System.out.println(i);
+        }
+
+        // select * from user_tb where id in (3,2,1) - distinct
+    }
+
+    @Test
+    public void findAll_lazyloading_test() {
+        List<Board> boardList = boardRepository.findAll();
+        boardList.forEach(board -> {
+            System.out.println(board.getUser().getUsername()); // lazy loading
+        });
+    }
+
+    @Test
     public void findAll_test() {
         // given
 
         // when
-        boardRepository.findAll();
+        List<Board> boardList = boardRepository.findAll();
 
         // then
     }
